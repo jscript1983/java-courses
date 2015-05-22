1fe0685d885b6546a84bfbe03dce0f442ae4834e
jscript1983 <eugenio.git@gmail.com>
2015-05-22 11:03:25 +0300

    user input

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+public class Calculator{
+    
+    /**
+        Результат вычисления
+    */
+    private int result;
+    
+    /**
+        Суммируем аргументы
+        @params params Аргументы суммирования.
+    */
+    public void add(int ... params){
+        for(Integer param : params){
+            this.result += param;
+        }
+    }
+    
+    /**
+        Получить результат
+        @return результат вычисления.
+    */
+    public int getResult(){
+        return this.result;
+    }
+    
+    /**
+        Очистить результат вычисления
+    */
+    public void cleanResult(){
+        this.result = 0;
+    }
+}
